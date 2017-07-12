f3855bd29a7de1686d6cb654cc7ac63a40c0fbc7
Alwin Egger <alwin.egger@tegonal.com>
2017-07-12 11:56:21 +0200

    omit install of grunt-cli

--- package.sh
+++ package.sh
@@ -92,7 +92,7 @@ do
 	  ( git clone --depth 1 -b $BRANCH https://github.com/OpenOlitor/${PROJECT}.git .tmp/${PROJECT} )
   fi
 
-  ( cd .tmp/${PROJECT} && npm install --cache-min 99999 && npm install -g grunt-cli && grunt build --env=$ENVIRONMENT --buildnr=$BUILD_NUMBER )
+  ( cd .tmp/${PROJECT} && npm install --cache-min 99999 && grunt build --env=$ENVIRONMENT --buildnr=$BUILD_NUMBER )
 
   PROJECT_NAME=$(echo $PROJECT | cut -d'-' -f 3)
   if [ "kundenportal" = $PROJECT_NAME ]; then
